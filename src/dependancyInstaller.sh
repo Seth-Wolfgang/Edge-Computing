df906ffa6124ce7c69255d013f0c9501b53f4a41
poockyy <wolfgangseth@gmail.com>
2022-04-26 16:32:33 -0400

    Fixed. It will now run the apt-get install commands.

--- src/dependancyInstaller.sh
+++ src/dependancyInstaller.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
 #Author: Seth Wolfgang
 #Date: 4/19/2022
+#Latest revision: 4/26/2022
 #For use in getting all required software for running of this project
-sudo apt-get install tesseract-ocr
-sudo apt-get install openjdk-17-jre
+sudo apt-get install tesseract-ocr -y
+sudo apt-get install openjdk-17-jre -y
 
