18679f6ce983b95a49b4e379fc9f7395c0d39098
poockyy <wolfgangseth@gmail.com>
2022-05-10 12:30:26 -0400

    Transferred client handling to ClientHandler.java. Now accepts new socket connections and creates new threads for each client.

--- src/Network/Server.java
+++ src/Network/Server.java
@@ -1,67 +1,42 @@
 package Network;
 
-import Benchmark.Timer;
-
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
 
 
 public class Server extends Thread {
 
     //initialize socket and input stream
-    private Socket          socket   = null;
-    private ServerSocket    server   = null;
-    private DataInputStream in       = null;
-    String line = "";
-    int counter = 0;
-    Timer timer = new Timer();
-    ArrayList<String> input = new ArrayList<>();
+    private ServerSocket server = null;
+    private DataInputStream in = null;
 
     // constructor with port
     public Server(int port) throws IOException {
 
+        server = new ServerSocket(port);
+        System.out.println("Server started");
+        System.out.println("Waiting for a client ...");
 
-        // starts server and waits for a connection
-        try {
 
-            server = new ServerSocket(port);
-            System.out.println("Server started");
-            System.out.println("Waiting for a client ...");
-            socket = server.accept();
-            System.out.println("Client accepted");
+        while (true) {
+            // starts server and waits for a connection
+            Socket socket = null;
 
-            in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            try {
 
-            boolean notRunning = true;
-            while (!socket.isClosed()) { //Replace with lambda
-                try {
-                    if (notRunning) {
-                        timer.start();
-                        notRunning = false;
-                    }
-                    line = in.readUTF();
-                    if (line.compareTo("") != 0 && !line.equals("over")) {
-                        counter++;
-                        timer.newLap();
-                        if (counter == 10) { //todo: this is a placeholder (replace with args)
-                            timer.stopTimer();
-                            stopServer(socket, in);
-                        }
-                    }
-                    else if(line.equals("over")){
-                        stopServer(socket, in);
-                    }
+                socket = server.accept();
+                System.out.println("Client accepted");
+                in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
 
-                } catch (IOException e) {
-                    throw new IOException(e);
-                }
+                Thread newClient = new ClientHandler(socket, in);
+                newClient.start();
+
+            } catch (IOException e) {
+                throw new IOException(e);
             }
-
-        } finally {
-            File results = new File("Results.txt");
-            timer.printResults("Transmission Received");
         }
     } //end of server
 
@@ -69,5 +44,14 @@ public class Server extends Thread {
         System.out.println("Closing connection");
         socket.close();
         in.close();
+    }
+
+    public void allowNewClient(Socket socket, DataInputStream in) {
+        try {
+            Thread clientThread = new Thread(new ClientHandler(socket, in));
+            clientThread.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
